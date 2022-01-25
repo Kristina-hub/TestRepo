50b6e57304e50d3d2848db1661b60cba239a78ca
Kristina Kacmarova <kkacmaro@uwo.ca>
2022-01-25 10:40:52 -0500

    html test

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import Flask
+# --------------------------------------- #
+#       Authors: Kristina Kacmarova       #
+#                Miranda Postma       	  #
+#                Ridwan Bari              #
+#                Winston Herold           #
+#       Python Version: 3.7.4             #
+#       Created on: 2022-01-18            #
+# --------------------------------------- #
+
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask.globals import request
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
-    return "Hello world!"
+    return render_template('index2.html')
+
+
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route("/")
+# def hello():
+#     return "Hello world!"
 
