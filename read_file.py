b34daf4f8ca13f9addfce38cd3852835d3b4bcd5
Kristina Kacmarova <kkacmaro@uwo.ca>
2022-01-25 20:49:46 -0500

    test

--- read_file.py
+++ read_file.py
@@ -12,8 +12,8 @@ from werkzeug.datastructures import FileStorage		#pip install werkzeug
 import PyPDF2										#pip install PyPDF2
 import tempfile
 from PIL import Image								#pip install Pillow
-#import pytesseract									#pip install pytesseract
-#from pdf2image import convert_from_path				#pip install pdf2image
+import pytesseract									#pip install pytesseract
+from pdf2image import convert_from_path				#pip install pdf2image
 import docx											#pip install docx  pip install python-docx
 
 class ReadFile():
@@ -28,21 +28,21 @@ class ReadFile():
 			text += pageObj.extractText()
 		return text
 					
-# 	def image_pdf(f, directory):
-# 		text = ""
-# 		pages = convert_from_path(directory + f.filename, 500)
-# 		image_counter = 1
-# 		for page in pages:
-# 			filename = "page_"+str(image_counter)+".jpg"
-# 			page.save(directory + filename, 'JPEG')
-# 			image_counter = image_counter + 1
-# 		filelimit = image_counter-1
-# 		for i in range(1, filelimit + 1):
-# 			filename = "page_"+str(i)+".jpg"
-# 			pagetext = str(((pytesseract.image_to_string(Image.open(directory + filename)))))
-# 			pagetext = pagetext.replace('-\n', '') 
-# 			text += pagetext 
-# 		return text
+	def image_pdf(f, directory):
+		text = ""
+		pages = convert_from_path(directory + f.filename, 500)
+		image_counter = 1
+		for page in pages:
+			filename = "page_"+str(image_counter)+".jpg"
+			page.save(directory + filename, 'JPEG')
+			image_counter = image_counter + 1
+		filelimit = image_counter-1
+		for i in range(1, filelimit + 1):
+			filename = "page_"+str(i)+".jpg"
+			pagetext = str(((pytesseract.image_to_string(Image.open(directory + filename)))))
+			pagetext = pagetext.replace('-\n', '') 
+			text += pagetext 
+		return text
 	
 	@staticmethod
 	def docx_file(f, directory):										#does not work for tables, headers, footers, foot notes
@@ -78,8 +78,8 @@ class ReadFile():
 			
 			elif (f.filename.split('.')[1] == "pdf"): 
 				text = ReadFile.text_pdf(f, directory) 						#text PDFs
-# 				if (text == ""): 
-# 					text = ReadFile.image_pdf(f, directory) 				#image PDFs
+				if (text == ""): 
+					text = ReadFile.image_pdf(f, directory) 				#image PDFs
 					
 		print("Exit: read_file.py")
 		return text
